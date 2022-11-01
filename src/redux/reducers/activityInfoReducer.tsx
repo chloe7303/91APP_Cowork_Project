ce05ffd49615f587644dac97e84838b622e6692f
chloe <chloehuang170@gmail.com>
2022-11-01 16:23:49 +0800

    change indent to spaces 2 from tab.

--- src/redux/reducers/activityInfoReducer.tsx
+++ src/redux/reducers/activityInfoReducer.tsx
@@ -1,41 +1,41 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 type initialStateType = {
-	userInfo: userInfoType;
-	phoneInfo: phoneInfoType;
+  userInfo: userInfoType;
+  phoneInfo: phoneInfoType;
 };
 export type userInfoType = {
-	phoneNumber?: string;
-	fullName?: string;
-	email?: string;
-	privacyAccept?: boolean;
-	areaCode?: string;
+  phoneNumber?: string;
+  fullName?: string;
+  email?: string;
+  privacyAccept?: boolean;
+  areaCode?: string;
 };
 type phoneInfoType = {
-	model?: string;
-	type?: string;
-	memory?: string;
-	quantity?: number;
-	price?: number;
+  model?: string;
+  type?: string;
+  memory?: string;
+  quantity?: number;
+  price?: number;
 };
 const initialState: initialStateType = {
-	userInfo:
-		JSON.parse(window.localStorage.getItem("userInfo") as string) ?? null,
-	phoneInfo:
-		JSON.parse(window.localStorage.getItem("phoneInfo") as string) ?? null,
+  userInfo:
+    JSON.parse(window.localStorage.getItem("userInfo") as string) ?? null,
+  phoneInfo:
+    JSON.parse(window.localStorage.getItem("phoneInfo") as string) ?? null,
 };
 
 const activityInfoSlice = createSlice({
-	name: "activityInfo",
-	initialState,
-	reducers: {
-		setUser: (state, action) => {
-			state.userInfo = action.payload.userInfo;
-		},
-		setPhoneInfo: (state, action) => {
-			state.phoneInfo = action.payload.phoneInfo;
-		},
-	},
+  name: "activityInfo",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.userInfo = action.payload.userInfo;
+    },
+    setPhoneInfo: (state, action) => {
+      state.phoneInfo = action.payload.phoneInfo;
+    },
+  },
 });
 
 const activityInfoReducer = activityInfoSlice.reducer;
