aab1808a17bbb36b0a6ab27817705cae2f67fc9d
chloe <chloehuang170@gmail.com>
2022-11-02 18:17:06 +0800

    create Slide component

---
+++ src/pages/products/sharedComponents/StyledItem.tsx
@@ -0,0 +1,17 @@
+const StyledItem = () => {
+  return (
+    <div className="flex justify-between p-5 pl-0 ml-5 border-b border-solid border-[#D8D9D8]">
+      <div className="w-[108px] h-[128px] bg-black mr-4"></div>
+      <div className="">
+        <h5 className="text-[14px] text-[#999999]">niko and ...</h5>
+        <h4>LOWRYS FARM素色飄逸垂墜感修身百褶長裙-三色</h4>
+        <small>NT$5</small>
+        <div>
+          <h4>NT$200</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StyledItem;
