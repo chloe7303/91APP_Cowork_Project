6c42bead6a7c88acf07f8774b9826d3ea5bb78c1
Kay Liaw <111190goodgoodeat@gmail.com>
2022-11-04 07:27:21 +0800

    product page layout

--- src/Products.tsx
+++ src/Products.tsx
@@ -1,5 +1,27 @@
+import Header from "./pages/products/sharedComponents/header/Header";
+import Footer from "./pages/products/sharedComponents/footer/Footer";
+import Main from "./pages/products/Main";
+import SideBar from "./pages/products/sharedComponents/sideBar/SideBar";
+import { data } from "./datas/products/data";
+import { storeInfoData } from "./datas/products/storeInfoData";
 function Products() {
-  return <div className="">products</div>;
+  return (
+    <div className="">
+      <Header />
+      <div className="w-full mx-auto max-w-[1180px] h-[48px] flex items-center">
+        <span className="text-[#C89185] text-[14px]">{`首頁 > 日本穿搭 > 穿搭介紹`}</span>
+      </div>
+      <div className="mt-10 flex w-full mx-auto max-w-[1180px]">
+        <div className="mr-[35px] max-w-[265px]">
+          <SideBar data={storeInfoData} />
+        </div>
+        <div className="mb-[260px] w-full mx-auto max-w-[1180px]">
+          <Main personData={data.styleHint} hintData={data.styleHint} />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Products;
