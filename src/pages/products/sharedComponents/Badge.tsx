c3471461e1c7a82cc0c3aec81e887e40f7770f62
chloe <chloehuang170@gmail.com>
2022-11-03 17:32:57 +0800

    fix badge align center

--- src/pages/products/sharedComponents/Badge.tsx
+++ src/pages/products/sharedComponents/Badge.tsx
@@ -1,7 +1,7 @@
 const Badge = ({ text }: { text: string | null }) => {
   return (
-    <div className="absolute bottom-[12px] left-[12px] rounded-[32px] text-light bg-[#666666] py-1.5 px-4 text-[14px]">
-      {text}
+    <div className="absolute bottom-[12px] left-[50%] -translate-x-[50%] rounded-[32px] text-light bg-[#666666] px-3.5 py-1.5 text-[14px]">
+      <span className="whitespace-nowrap">{text}</span>
     </div>
   );
 };
